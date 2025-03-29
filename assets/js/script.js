678699a64c6a4744aeb0bfabae02e80e9253e755
Dave-MK <davidmkilgallon@gmail.com>
2025-03-29 22:58:01 +0000

    Add code to calculate correct answer for addition

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,50 +10,75 @@ document.addEventListener("DOMContentLoaded", function () {
 				alert("You clicked submit!");
 			} else {
 				let gameType = this.getAttribute("data-type");
-				alert(`You clicked ${gameType}`);
+				runGame(gameType);
 			}
 		})
 	}
+
+	runGame("addition");
 })
 
 /**
  * The main game loop, which will run when the game starts
  */
-function runGame() {
+function runGame(gameType) {
 
 	// The random numbers should be between 1 and 25
 	let num1 = Math.floor(Math.random() * 25) + 1;
 	let num2 = Math.floor(Math.random() * 25) + 1;
+
+	if (gameType === "addition") {
+		displayAdditionQuestion(num1, num2);
+	} else {
+		alert(`Unknown game type: ${gameType}`);
+		throw `Unknown game type: ${gameType}. Aborting!`;
+	}
 }
 
 function checkAnswer() {
 
 }
 
+/**
+ * Fetches the operands and operator for the question from the DOM
+ * Checks the answer against the value in the user's answer box for correctness
+ */
 function calculateCorrectAnswer() {
+	let operand1 = parseInt(document.getElementById("operand1").textContent);
+	let operand2 = parseInt(document.getElementById("operand2").textContent);
+	let operator = document.getElementById("operator").textContent;
 
-}
+	let correctAnswer = 0;
 
-function incrementScore() {
+	if (operator === "+") {
+		return [operand1 + operand2, "addition"];
+	} else {
+		alert(`Unknown operator: ${operator}`);
+		throw `Unknown operator: ${operator}. Aborting!`;
+	}
 
-}
+	function incrementScore() {
 
-function incrementWrongAnswer() {
+	}
 
-}
+	function incrementWrongAnswer() {
 
-function displayAdditionQuestion() {
+	}
 
-}
+	function displayAdditionQuestion(operand1, operand2) {
+		document.getElementById("operand1").textContent = operand1;
+		document.getElementById("operand2").textContent = operand2;
+		document.getElementById("operator").textContent = "+";
+	}
 
-function displaySubtractQuestion() {
+	function displaySubtractQuestion() {
 
-}
+	}
 
-function displayMultiplyQuestion() {
+	function displayMultiplyQuestion() {
 
-}
+	}
 
-function displayDivisionQuestion() {
+	function displayDivisionQuestion() {
 
-}
+	}
