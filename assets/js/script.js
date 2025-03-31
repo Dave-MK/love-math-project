97345654a778b5e66b90067867b9777dba7c181f
Dave-MK <davidmkilgallon@gmail.com>
2025-04-01 00:01:39 +0100

    Add code to update subtraction questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -110,19 +110,19 @@ function displayAdditionQuestion(operand1, operand2) {
 }
 
 function displaySubtractQuestion(operand1, operand2) {
-	document.getElementById("operand1").textContent = operand1;
-	document.getElementById("operand2").textContent = operand2;
+	document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+	document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
 	document.getElementById("operator").textContent = "-";
 }
 
-function displayMultiplyQuestion() {
+function displayMultiplyQuestion(operand1, operand2) {
 	document.getElementById("operand1").textContent = operand1;
 	document.getElementById("operand2").textContent = operand2;
 	document.getElementById("operator").textContent = "x";
 }
 
-function displayDivisionQuestion() {
-	document.getElementById("operand1").textContent = operand1;
-	document.getElementById("operand2").textContent = operand2;
+function displayDivisionQuestion(operand1, operand2) {
+	document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand1;
+	document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
 	document.getElementById("operator").textContent = "/";
 }
