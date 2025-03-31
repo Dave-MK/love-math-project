be07be11def239fa4b8b13b46e2f73bd163f0f7f
Dave-MK <davidmkilgallon@gmail.com>
2025-03-31 10:34:47 +0100

    Add code to update the scores for correct and incorrect answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -75,12 +75,20 @@ function calculateCorrectAnswer() {
 	}
 }
 
+/**
+ * Reads old correct score and updates it with the new score if answer is correct
+ */
 function incrementScore() {
-
+	let oldScore = parseInt(document.getElementById("score").innerText);
+	document.getElementById("score").innerText = ++oldScore;
 }
 
+/**
+ * Reads old incorrect score and updates it with the new score if answer is incorrect
+ */
 function incrementWrongAnswer() {
-
+	let oldScore = parseInt(document.getElementById("incorrect").innerText);
+	document.getElementById("incorrect").innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2) {
