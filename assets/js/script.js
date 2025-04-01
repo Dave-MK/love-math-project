66033f8d2a9ff8ba4c14261a94b9fc27dc633396
Dave-MK <davidmkilgallon@gmail.com>
2025-04-01 01:34:01 +0100

    Tidy up division and finalise

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,6 +37,7 @@ function runGame(gameType) {
 	let num1 = Math.floor(Math.random() * 25) + 1;
 	let num2 = Math.floor(Math.random() * 25) + 1;
 
+
 	if (gameType === "addition") {
 		displayAdditionQuestion(num1, num2);
 	} else if (gameType === "subtract") {
@@ -131,7 +132,7 @@ function displayMultiplyQuestion(operand1, operand2) {
 }
 
 function displayDivisionQuestion(operand1, operand2) {
-	document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand1;
-	document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+	document.getElementById("operand1").textContent = operand1 > operand2 && operand1 % operand2 === 0 ? operand1 : operand1;
+	document.getElementById("operand2").textContent = operand1 > operand2 && operand1 % operand2 === 0 ? operand2 : operand1;
 	document.getElementById("operator").textContent = "/";
 }
